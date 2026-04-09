0029301302d858999313b90a427b1db2f204a605
brenden <brenden@theehlers.org>
2026-04-08 20:56:23 -0600

    axum askama setup

--- leaderboard/src/main.rs
+++ leaderboard/src/main.rs
@@ -1,5 +1,22 @@
+use axum::{Router, routing::get};
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let app = Router::new().route("/", get(root));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
     Ok(())
+}
+
+async fn root<'a>() -> HelloTemplate<'a> {
+    let hello = HelloTemplate {
+        name: "Evil Elaina >:(",
+    };
+    hello
+}
+
+#[derive(askama::Template, askama_web::WebTemplate)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
 }
